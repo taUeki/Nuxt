8d6f79232adb4b1730076edb17a239f56564bc10
植木孝史 <>
2020-09-09 17:24:59 +0900

    aaaaa

---
+++ app/pages/hoge/index.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="section">
+  <div class="field">
+    <label class="label">タイトル</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="タイトルを入力">
+    </div>
+  </div>
+  <div class="field">
+    <label class="label">リソースURL</label>
+    <div class="control">
+      <input class="input" type="url" placeholder="リソースURLを入力">
+    </div>
+  </div>
+  <div class="field">
+    <label class="label">記事テキスト</label>
+    <div class="control">
+      <textarea class="textarea" placeholder="記事を入力"></textarea>
+    </div>
+  </div>
+  <div class="field is-grouped">
+    <div class="control">
+      <button class="button is-link">登録</button>
+    </div>
+    <div class="control">
+      <button class="button is-link is-light">キャンセル</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
